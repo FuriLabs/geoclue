87efd9a932cf9b723d6fc84f7b61ebb4cc09d635
Chris Talbot <chris@talbothome.com>
2024-12-01 21:34:44 -0700

    Mozilla: replace rather than append User-Agent
    It won't actually attach if you don't do this

--- src/gclue-mozilla.c
+++ src/gclue-mozilla.c
@@ -301,7 +301,7 @@ gclue_mozilla_create_query (GClueMozilla  *mozilla,
         uri = gclue_mozilla_get_locate_url (mozilla);
         ret = soup_message_new ("POST", uri);
         request_headers = soup_message_get_request_headers (ret);
-        soup_message_headers_append (request_headers, "User-Agent", USER_AGENT);
+        soup_message_headers_replace (request_headers, "User-Agent", USER_AGENT);
         body = g_bytes_new_take (data, data_len);
         soup_message_set_request_body_from_bytes (ret, "application/json", body);
         g_debug ("Sending following request to '%s':\n%s", uri, data);
@@ -566,7 +566,7 @@ gclue_mozilla_create_submit_query (GClueMozilla  *mozilla,
 
         ret = soup_message_new ("POST", url);
         request_headers = soup_message_get_request_headers (ret);
-        soup_message_headers_append (request_headers, "User-Agent", USER_AGENT);
+        soup_message_headers_replace (request_headers, "User-Agent", USER_AGENT);
         if (nick != NULL && nick[0] != '\0')
                 soup_message_headers_append (request_headers,
                                              "X-Nickname",
